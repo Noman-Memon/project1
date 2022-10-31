addf58a50d8a572f2fcc9ac61e6fa3a7e5ff4086
ABASITJAMALBROTHERS <noman.rauf@jamalbrothers.com.pk>
2022-10-31 19:08:15 +0500

    set body backgroungd color

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,19 @@
 import React from 'react'
+import '../style.css'
 
 export default function Main() {
-  return <div>hwllo</div>
+  return (
+    <div>
+      <main>
+        <h1 className="main_h1">Fun Facts About React</h1>
+        <ul className="main_ui">
+          <li>Was First Released in 2013</li>
+          <li>Was originaly created by ------</li>
+          <li>Was well over 100k stars on github</li>
+          <li>Is metained by facebook</li>
+          <li>Powers thousands of enterprise apps including Mobile apps</li>
+        </ul>
+      </main>
+    </div>
+  )
 }
