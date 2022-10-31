66eec2446193071a5d04371f092a990f7cf335a5
ABASITJAMALBROTHERS <noman.rauf@jamalbrothers.com.pk>
2022-10-31 14:40:37 +0500

    Create Style.css

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Logo from '../image/react-logo.png'
+import '../style.css'
+
+export default function s() {
+  return (
+    <div>
+      <nav>
+        <img src={Logo} alt="react-logo"></img>
+        <h3>ReactFacts</h3>
+        <h4>React Course-Project 1</h4>
+      </nav>
+    </div>
+  )
+}
